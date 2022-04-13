b4ab353469526d4f1be5fd987ea1dd8aad37c257
DEVIL-RO <103538922+DEVIL-RO@users.noreply.github.com>
2022-04-13 23:25:34 +0300

    Update SocialMediaIcons.tsx

--- components/SocialMediaIcons.tsx
+++ components/SocialMediaIcons.tsx
@@ -5,17 +5,27 @@ export const SocialMediaIcons = () => {
   return (
     <Box display="flex" alignItems="center" gap={3}>
       <SocialIcon
-        url="https://www.twitter.com/ElvenTools"
+        url="https://discord.gg/4Xsgr4maGV"
         bgColor="#fff"
         style={{ width: 30, height: 30 }}
       />
       <SocialIcon
-        url="https://www.github.com/ElvenTools"
+        url="https://twitter.com/DevilroNFT"
         bgColor="#fff"
         style={{ width: 30, height: 30 }}
       />
       <SocialIcon
-        url="https://www.telegram.me/juliancwirko"
+        url="https://www.instagram.com/elrond.devils/"
+        bgColor="#fff"
+        style={{ width: 30, height: 30 }}
+      />
+      <SocialIcon
+        url="https://devil.ro/#"
+        bgColor="#fff"
+        style={{ width: 30, height: 30 }}
+      />
+      <SocialIcon
+        url="https://youtube.com/channel/UCYV5_SUtLWwnuyyWwaXAYNA"
         bgColor="#fff"
         style={{ width: 30, height: 30 }}
       />
